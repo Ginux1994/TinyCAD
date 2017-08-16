6fe37d594b4fc04a92f9ae63aa89bf271b2c5216
sdoa5335717 <sdoa5335717@163.com>
2017-08-16 17:45:13 +0800

    add a test python script

---
+++ TinyCAD/mapinfo.py
@@ -0,0 +1,9 @@
+# -*- coding: cp936 -*-
+def mapinfo():
+    maps = [1,'mapfile']
+    data = [['jinan', [100, 100], 124],
+            ['beijing', [120.11, 124.12],57],
+            ['tianjin', [123.15, 222.45],200]]
+    return data
+if __name__ == '__main__':
+    mapinfo()
